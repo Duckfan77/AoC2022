d92ea50bfaad74df1a43e3eb32a5dad6c63b473c
Melissa <77photon@gmail.com>
2022-11-30 21:20:37 -0600

    First framework for AoC day 1

---
+++ day01/src/main.rs
@@ -0,0 +1,12 @@
+use std::fs::File;
+use std::io::prelude::*;
+
+fn main() {
+    let mut file: File = File::open("input").unwrap();
+    let mut text = String::new();
+    file.read_to_string(&mut text).unwrap();
+
+    println!("Part 1:");
+
+    println!("\nPart 2:");
+}
