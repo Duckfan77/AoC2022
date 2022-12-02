a1957a2ce2af3671e8969aea38cf9e204aa02179
Melissa <77photon@gmail.com>
2022-12-02 08:16:05 -0600

    day02 part 1

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -13,6 +13,24 @@ fn main() {
     part2(&text);
 }
 
-fn part1(_text: &String) {}
+fn part1(text: &String) {
+    println!(
+        "{}",
+        text.lines()
+            .map(|line| match line {
+                "A X" => 1 + 3,
+                "A Y" => 2 + 6,
+                "A Z" => 3 + 0,
+                "B X" => 1 + 0,
+                "B Y" => 2 + 3,
+                "B Z" => 3 + 6,
+                "C X" => 1 + 6,
+                "C Y" => 2 + 0,
+                "C Z" => 3 + 3,
+                _ => unreachable!("Invalid line: {}\n", line),
+            })
+            .sum::<i64>()
+    )
+}
 
 fn part2(_text: &String) {}
