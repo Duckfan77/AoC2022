81dc3ebb6bafc3e8a04ac1c787d30b717111a714
Melissa <77photon@gmail.com>
2022-12-03 19:45:16 -0600

    day04 platform setup

---
+++ day04/src/main.rs
@@ -0,0 +1,19 @@
+use itertools::Itertools;
+use std::fs::File;
+use std::io::prelude::*;
+
+fn main() {
+    let mut file: File = File::open("input").unwrap();
+    let mut text = String::new();
+    file.read_to_string(&mut text).unwrap();
+
+    println!("Part 1:");
+    part1(&text);
+
+    println!("\nPart 2:");
+    part2(&text);
+}
+
+fn part1(_text: &String) {}
+
+fn part2(_text: &String) {}
