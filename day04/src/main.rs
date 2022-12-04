58acccf7e57bc9d7a41fe475728f388a90c65733
Melissa <77photon@gmail.com>
2022-12-03 23:12:35 -0600

    day04 part 1

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -14,6 +14,37 @@ fn main() {
     part2(&text);
 }
 
-fn part1(_text: &String) {}
+#[derive(Debug)]
+struct Range {
+    l: i32,
+    r: i32,
+}
+
+impl From<&str> for Range {
+    fn from(item: &str) -> Self {
+        let (strl, strr) = item.split_once("-").expect("item should have dash");
+        Self {
+            l: strl.parse().unwrap(),
+            r: strr.parse().unwrap(),
+        }
+    }
+}
+
+fn contains(r1: Range, r2: Range) -> bool {
+    (r1.l <= r2.l && r1.r >= r2.r) || (r2.l <= r1.l && r2.r >= r1.r)
+}
+
+fn part1(text: &String) {
+    println!(
+        "{}",
+        text.lines()
+            .map(|l| {
+                let (l, r) = l.split_once(",").unwrap();
+                (l, r)
+            })
+            .filter(|(l, r)| contains((*l).into(), (*r).into()))
+            .count()
+    );
+}
 
 fn part2(_text: &String) {}
