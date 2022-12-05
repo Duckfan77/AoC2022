f25e58cc157ac24c80383acea65e82de2a475e82
Melissa <77photon@gmail.com>
2022-12-04 23:14:53 -0600

    day05 part 1

--- day05/src/main.rs
+++ day05/src/main.rs
@@ -14,6 +14,59 @@ fn main() {
     part2(&text);
 }
 
-fn part1(text: &String) {}
+fn get_start() -> Vec<Vec<char>> {
+    vec![
+        vec![], // 1 indexed, just do this to make it simpler
+        vec!['F', 'C', 'P', 'G', 'Q', 'R'],
+        vec!['W', 'T', 'C', 'P'],
+        vec!['B', 'H', 'P', 'M', 'C'],
+        vec!['L', 'T', 'Q', 'S', 'M', 'P', 'R'],
+        vec!['P', 'H', 'J', 'Z', 'V', 'G', 'N'],
+        vec!['D', 'P', 'J'],
+        vec!['L', 'G', 'P', 'Z', 'F', 'J', 'T', 'R'],
+        vec!['N', 'L', 'H', 'C', 'F', 'P', 'T', 'J'],
+        vec!['G', 'V', 'Z', 'Q', 'H', 'T', 'C', 'W'],
+    ]
+}
 
-fn part2(text: &String) {}
+fn move_crate(stacks: &mut Vec<Vec<char>>, start: usize, end: usize, count: usize) {
+    for _ in 0..count {
+        let c = stacks[start].pop().expect("popped from empty stack");
+        stacks[end].push(c);
+    }
+}
+
+fn parse_line(line: &str) -> (usize, usize, usize) {
+    let parts = line.split_whitespace().collect::<Vec<_>>();
+    (
+        parts[1].parse().unwrap(),
+        parts[3].parse().unwrap(),
+        parts[5].parse().unwrap(),
+    )
+}
+
+fn move_crates_group(stacks: &mut Vec<Vec<char>>, start: usize, end: usize, count: usize) {}
+
+fn part1(text: &String) {
+    let mut stacks = get_start();
+    for line in text.lines() {
+        let (count, start, end) = parse_line(line);
+        move_crate(&mut stacks, start, end, count);
+    }
+    for stack in stacks {
+        print!("{}", stack.last().unwrap_or(&' '));
+    }
+    println!();
+}
+
+fn part2(text: &String) {
+    let mut stacks = get_start();
+    for line in text.lines() {
+        let (count, start, end) = parse_line(line);
+        move_crates_group(&mut stacks, start, end, count);
+    }
+    for stack in stacks {
+        print!("{}", stack.last().unwrap_or(&' '));
+    }
+    println!();
+}
