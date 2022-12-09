5ba9762b49bdadb8ce6dc7b08292710cb9885d77
Melissa <77photon@gmail.com>
2022-12-08 23:30:24 -0600

    day09 part 2

--- day09/src/main.rs
+++ day09/src/main.rs
@@ -104,4 +104,77 @@ fn part1(text: &String) {
     println!("{}", set.len())
 }
 
-fn part2(text: &String) {}
+struct GenRope {
+    knots: Vec<(i64, i64)>,
+}
+
+impl GenRope {
+    fn new(len: usize) -> Self {
+        Self {
+            knots: vec![(0, 0); len],
+        }
+    }
+
+    fn update_knot(&mut self, i: usize) {
+        let xdif = self.knots[i - 1].0 - self.knots[i].0;
+        let ydif = self.knots[i - 1].1 - self.knots[i].1;
+
+        if ydif.abs() < 2 && xdif.abs() < 2 {
+            return; // early return, nothing to do.
+        }
+
+        if xdif == 0 {
+            // move on y
+            self.knots[i].1 += ydif / 2; // ydif must be +-2
+        } else if ydif == 0 {
+            self.knots[i].0 += xdif / 2; // xdif must be +-2
+        } else {
+            // move on both
+            if xdif.abs() == 2 && ydif.abs() == 2 {
+                self.knots[i].0 += xdif / 2;
+                self.knots[i].1 += ydif / 2;
+            } else if xdif.abs() == 2 {
+                // xdif.abs() = 2 and ydif.abs() = 1
+                self.knots[i].0 += xdif / 2;
+                self.knots[i].1 += ydif;
+            } else {
+                // xdif.abs() = 1 and ydif.abs() = 2
+                self.knots[i].0 += xdif;
+                self.knots[i].1 += ydif / 2;
+            }
+        }
+    }
+
+    fn update_tail(&mut self) {
+        for i in 1..self.knots.len() {
+            self.update_knot(i);
+        }
+    }
+
+    fn move_head(&mut self, dir: Direction) -> (i64, i64) {
+        match dir {
+            Direction::Up => self.knots[0].1 += 1,
+            Direction::Left => self.knots[0].0 -= 1,
+            Direction::Down => self.knots[0].1 -= 1,
+            Direction::Right => self.knots[0].0 += 1,
+        }
+
+        self.update_tail();
+
+        *self.knots.last().unwrap()
+    }
+}
+
+fn part2(text: &String) {
+    let mut set = HashSet::new();
+    let mut rope = GenRope::new(10);
+    set.insert((0, 0));
+    for line in text.lines() {
+        let (d, i) = Direction::parse(line);
+        for _ in 0..i {
+            set.insert(rope.move_head(d));
+        }
+    }
+
+    println!("{}", set.len())
+}
