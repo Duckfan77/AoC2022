028e3a44b11da15329f0aa7d994cac46598b09bc
Melissa <77photon@gmail.com>
2022-12-12 23:33:44 -0600

    day13 part 1

--- day13/src/main.rs
+++ day13/src/main.rs
@@ -1,3 +1,4 @@
+use serde_json::Value;
 use std::fs::File;
 use std::io::prelude::*;
 
@@ -13,6 +14,81 @@ fn main() {
     part2(&text);
 }
 
-fn part1(text: &String) {}
+struct Pair {
+    p1: Packet,
+    p2: Packet,
+}
+
+impl Pair {
+    fn part1_right(&self) -> bool {
+        self.p1 < self.p2
+    }
+}
+
+enum Packet {
+    Num(i64),
+    List(Vec<Packet>),
+}
+
+impl PartialEq for Packet {
+    fn eq(&self, other: &Self) -> bool {
+        match (self, other) {
+            (Self::Num(l0), Self::Num(r0)) => l0 == r0,
+            (Self::List(l0), Self::List(r0)) => l0 == r0,
+            (Self::Num(l0), Self::List(_)) => Packet::List(vec![Packet::Num(*l0)]) == *other,
+            (Self::List(_), Self::Num(r0)) => *self == Packet::List(vec![Packet::Num(*r0)]),
+        }
+    }
+}
+
+impl PartialOrd for Packet {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        match (self, other) {
+            (Packet::Num(i1), Packet::Num(i2)) => i1.partial_cmp(i2),
+            (Packet::Num(i1), Packet::List(_)) => {
+                Packet::List(vec![Packet::Num(*i1)]).partial_cmp(other)
+            }
+            (Packet::List(_), Packet::Num(i1)) => {
+                self.partial_cmp(&Packet::List(vec![Packet::Num(*i1)]))
+            }
+            (Packet::List(l1), Packet::List(l2)) => l1.partial_cmp(l2),
+        }
+    }
+}
+
+impl Packet {
+    fn from_val(v: &Value) -> Packet {
+        match v {
+            Value::Null => unimplemented!("Null hit"),
+            Value::Bool(_) => unimplemented!("bool hit"),
+            Value::Number(i) => Packet::Num(i.as_i64().unwrap()),
+            Value::String(_) => unimplemented!("string hit"),
+            Value::Array(a) => Packet::List(a.into_iter().map(|v1| Packet::from_val(v1)).collect()),
+            Value::Object(_) => unimplemented!("object hit"),
+        }
+    }
+}
+
+fn part1(text: &String) {
+    let pairs = text
+        .split("\n\n")
+        .map(|block| {
+            let (l1, l2) = block.split_once("\n").unwrap();
+            Pair {
+                p1: Packet::from_val(&serde_json::from_str(l1).unwrap()),
+                p2: Packet::from_val(&serde_json::from_str(l2).unwrap()),
+            }
+        })
+        .collect::<Vec<Pair>>();
+
+    println!(
+        "{}",
+        pairs
+            .iter()
+            .enumerate()
+            .filter_map(|(i, p)| if p.part1_right() { Some(i + 1) } else { None })
+            .sum::<usize>()
+    );
+}
 
 fn part2(text: &String) {}
